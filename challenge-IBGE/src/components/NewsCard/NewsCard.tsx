8d9fe3cbe95726535d6131ab4d7744600dc77cb9
Danrllei Miranda <danmiranda14@gmail.com>
2023-10-11 12:04:07 -0300

    favoritar e estilização

--- challenge-IBGE/src/components/NewsCard/NewsCard.tsx
+++ challenge-IBGE/src/components/NewsCard/NewsCard.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { NewsType } from "../../types";
+import style from "./newscard.module.css";
 
 type NewsCardProps = {
   news: NewsType;
@@ -7,20 +7,75 @@ type NewsCardProps = {
 };
 
 export default function NewsCard({ news, index }: NewsCardProps) {
-  const image = JSON.parse(news.imagens);
+  const publishedDaysAgo = () => {
+    const dataBrasileira = news.data_publicacao;
+
+    const partesDataHora = dataBrasileira.split(/[\s:/]+/);
+
+    const dataParaComparar = new Date(
+      parseInt(partesDataHora[2], 10),
+      parseInt(partesDataHora[1], 10) - 1,
+      parseInt(partesDataHora[0], 10),
+      parseInt(partesDataHora[3], 10),
+      parseInt(partesDataHora[4], 10),
+      parseInt(partesDataHora[5], 10)
+    );
+
+    const dataAtual = new Date();
+
+    const diferencaEmMilissegundos =
+      dataAtual.getTime() - dataParaComparar.getTime();
+
+    const diferencaEmDias = diferencaEmMilissegundos / (1000 * 60 * 60 * 24);
+
+    if (Math.floor(diferencaEmDias) === 0) {
+      return "Hoje";
+    }
+
+    if (Math.floor(diferencaEmDias) === 1) {
+      return "1 dia atrás";
+    }
+
+    if (Math.floor(diferencaEmDias) > 1) {
+      return `${Math.floor(diferencaEmDias)} dias atrás`;
+    }
+  };
+
+  const handleFavorite = () => {
+    const alreadyInLocalStorage = JSON.parse(
+      localStorage.getItem("favoriteNews") || "[]"
+    );
+    const favoriteNews = [...alreadyInLocalStorage, news];
+    localStorage.setItem("favoriteNews", JSON.stringify(favoriteNews));
+
+    if (alreadyInLocalStorage.some((item: NewsType) => item.id === news.id)) {
+      const filteredNews = alreadyInLocalStorage.filter(
+        (item: NewsType) => item.id !== news.id
+      );
+      localStorage.setItem("favoriteNews", JSON.stringify(filteredNews));
+    }
+  };
+
   return (
-    <>
+    <div className={style.card}>
+      <div className={style.cardTop}>
+        {index === 0 && <span>Notícia mais recente</span>}
+        {index === 0 && <button onClick={handleFavorite}>Favoritar</button>}
+      </div>
       <h3>{news.titulo}</h3>
-      {index === 0 && (
-        <img
-          src={`https://agenciadenoticias.ibge.gov.br/${image.image_intro}`}
-          alt=""
-        />
-      )}
+
       <p>{news.introducao}</p>
-      <a href={news.link} target="blank">
-        Leia a notícia aqui
-      </a>
-    </>
+      <div className={style.cardBottom}>
+        <span>{`${publishedDaysAgo()}`}</span>
+        <a href={news.link} target="blank">
+          Leia a notícia aqui
+        </a>
+      </div>
+      {index >= 1 && (
+        <button onClick={handleFavorite} className={style.bottomBtn}>
+          Favoritar
+        </button>
+      )}
+    </div>
   );
 }
--- challenge-IBGE/src/components/NewsList/NewsList.tsx
+++ challenge-IBGE/src/components/NewsList/NewsList.tsx
@@ -3,6 +3,7 @@ import { Dispatch, GlobalStateType, NewsType } from "../../types";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../../redux/actions";
 import NewsCard from "../NewsCard/NewsCard";
+import style from "./newslist.module.css";
 
 export default function () {
   const dispatch: Dispatch = useDispatch();
@@ -13,12 +14,25 @@ export default function () {
     (globalState: GlobalStateType) => globalState.news.items
   );
 
+  const image = JSON.parse(IBGE_news[0].imagens);
+
   return (
     <div>
-      {IBGE_news &&
-        IBGE_news.map((news: NewsType, index) => (
-          <NewsCard key={index} news={news} index={index} />
-        ))}
+      <div className={style.firstNews}>
+        <img
+          src={`https://agenciadenoticias.ibge.gov.br/${image.image_intro}`}
+          alt=""
+        />
+        {IBGE_news && <NewsCard news={IBGE_news[0]} index={0} />}
+      </div>
+      <div className={style.container}>
+        {IBGE_news &&
+          IBGE_news.filter((_, index) => index > 0).map(
+            (news: NewsType, index) => (
+              <NewsCard key={index} news={news} index={index + 1} />
+            )
+          )}
+      </div>
     </div>
   );
 }
