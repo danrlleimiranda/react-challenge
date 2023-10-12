7842a840c524d10625ff28110bb871be45b7c587
Danrllei Miranda <danmiranda14@gmail.com>
2023-10-12 13:18:24 -0300

    atualização

--- challenge-IBGE/src/components/NewsList/NewsList.tsx
+++ challenge-IBGE/src/components/NewsList/NewsList.tsx
@@ -1,38 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Dispatch, GlobalStateType, NewsType } from "../../types";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../../redux/actions";
 import NewsCard from "../NewsCard/NewsCard";
 import style from "./newslist.module.css";
+import Filters from "../Filters/Filters";
 
 export default function () {
-  const dispatch: Dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchData());
-  }, []);
   const IBGE_news = useSelector(
     (globalState: GlobalStateType) => globalState.news.items
   );
 
-  const image = JSON.parse(IBGE_news[0].imagens);
+  const [IBGE, setIBGE] = useState<NewsType[]>(IBGE_news);
+  const dispatch: Dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
+  const [quantityNews, setQuantityNews] = useState(7);
+
+  const handleNewsQuantity = () => {
+    setQuantityNews((prevQuantity) => prevQuantity + 6);
+  };
+
+  const image = IBGE_news ? JSON.parse(IBGE_news[0].imagens) : "";
 
   return (
-    <div>
-      <div className={style.firstNews}>
+    <main>
+      <section className={style.firstNews}>
         <img
           src={`https://agenciadenoticias.ibge.gov.br/${image.image_intro}`}
           alt=""
         />
-        {IBGE_news && <NewsCard news={IBGE_news[0]} index={0} />}
-      </div>
-      <div className={style.container}>
+        <div>
+          {IBGE_news && (
+            <NewsCard news={IBGE_news[0]} index={0} className="cardFirstNews" />
+          )}
+        </div>
+      </section>
+      <Filters />
+      <section className={style.container}>
         {IBGE_news &&
-          IBGE_news.filter((_, index) => index > 0).map(
+          IBGE_news.filter((_, index) => index > 0 && index < quantityNews).map(
             (news: NewsType, index) => (
-              <NewsCard key={index} news={news} index={index + 1} />
+              <NewsCard
+                key={index}
+                news={news}
+                index={index + 1}
+                className="card"
+              />
             )
           )}
-      </div>
-    </div>
+        <button onClick={handleNewsQuantity} className={style.showMore}>
+          Mais notícias
+        </button>
+      </section>
+    </main>
   );
 }
