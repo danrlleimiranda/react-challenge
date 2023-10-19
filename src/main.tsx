9540e243e437eb588306b99b337aefc81054ceeb
Danrllei Miranda <danmiranda14@gmail.com>
2023-10-18 22:45:58 -0300

    pre-deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
-import store from './redux/index.ts';
+import store from './redux/index';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter basename="/">
     <Provider store={ store }>
       <App />
     </Provider>
     ,
-  </BrowserRouter>,
+  </HashRouter>,
 );
