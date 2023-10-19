9a3ea24bb972217e0d45beda0bf6ccb3c2b8764d
Danrllei Miranda <danmiranda14@gmail.com>
2023-10-19 12:14:26 -0300

    finalizado

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import store from './redux/index';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/">
+  <HashRouter basename="/">
     <Provider store={ store }>
       <App />
     </Provider>
     ,
-  </BrowserRouter>,
+  </HashRouter>,
 );
